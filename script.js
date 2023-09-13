49e39b611f1659b922052d37f39acc166951cb01
Srikant <srikantmishra856@gmail.com>
2023-09-13 21:00:32 +0530

    did some changes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function myFunction() {
     var x = document.getElementById("myTopnav");
     var menu = document.querySelector(".menu");
     var header = document.querySelector(".header");
-  
+
     if (x.className === "navbar") {
         x.className += " responsive";
         menu.style.display = "flex";
@@ -13,12 +13,16 @@ function myFunction() {
         menu.style.display = "none";
         header.style.marginTop = 0;
     }
-  }
-  
-  // Smooth scrolling when clicking on menu items with the class "smooth-scroll"
-  document.addEventListener('DOMContentLoaded', function () {
+}
+
+function toggleMenu(x) {
+    x.classList.toggle("change");
+}
+
+// Smooth scrolling when clicking on menu items with the class "smooth-scroll"
+document.addEventListener('DOMContentLoaded', function () {
     const menuLinks = document.querySelectorAll('.smooth-scroll');
-  
+
     menuLinks.forEach(link => {
         link.addEventListener('click', function (e) {
             e.preventDefault();
@@ -26,18 +30,18 @@ function myFunction() {
             const headerOffset = 75; // Adjust this value based on your header height
             const elementPosition = targetSection.getBoundingClientRect().top;
             const offsetPosition = elementPosition - headerOffset;
-  
+
             window.scrollBy({
                 top: offsetPosition,
                 behavior: 'smooth'
             });
         });
     });
-  });
-  
-  document.addEventListener("DOMContentLoaded", function () {
+});
+
+document.addEventListener("DOMContentLoaded", function () {
     const skillProgressBars = document.querySelectorAll(".progress");
-  
+
     // Function to update skill progress based on intersection
     function updateSkillProgress(entries) {
         entries.forEach(entry => {
@@ -47,14 +51,14 @@ function myFunction() {
             }
         });
     }
-  
+
     // Create an Intersection Observer 
     const observer = new IntersectionObserver(updateSkillProgress, {
         root: null,
         rootMargin: "0px",
         threshold: 0.1 // 10% of the element is visible
     });
-  
+
     // Observe each skill progress bar
     skillProgressBars.forEach(bar => observer.observe(bar));
-  });
+});
